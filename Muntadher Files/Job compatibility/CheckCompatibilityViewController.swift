79c10c6347d947f99adab95f2978717de273079b
Qassimkiller231 <Qassimahmed231@gmail.com>
2024-12-22 02:05:26 +0300

    almost finished integrating the application history page

--- Muntadher Files/Job compatibility/CheckCompatibilityViewController.swift
+++ Muntadher Files/Job compatibility/CheckCompatibilityViewController.swift
@@ -7,24 +7,21 @@
 
 import UIKit
 
-protocol CheckCompatibilityDelegate: AnyObject {
-    func didSelectOption(_ option: String)
-}
 
-class CheckCompatibilityViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CheckCompatibilityDelegate {
-    func didSelectOption(_ option: String) {
-        <#code#>
-    }
-    
+
+class CheckCompatibilityViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var model: [table] = tableData
     @IBOutlet weak var tableView: UITableView!
 
+    @IBOutlet weak var CheckResultButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(DropDownTableViewCell.nib(), forCellReuseIdentifier: DropDownTableViewCell.identifier)
+//        tableView.backgroundColor = .clear
     }
 
     // MARK: - TableView Methods
@@ -40,36 +37,51 @@ class CheckCompatibilityViewController: UIViewController, UITableViewDelegate, U
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: DropDownTableViewCell.identifier) as! DropDownTableViewCell
-            cell.configure(with: model[indexPath.section].title)
+            let title = model[indexPath.section].selectedOption ?? model[indexPath.section].title
+            cell.configure(with: title)
+//            cell.backgroundColor = .clear
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "Options", for: indexPath)
             cell.textLabel?.text = model[indexPath.section].options[indexPath.row - 1]
+            cell.backgroundColor = .clear
             return cell
         }
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.row == 0 {
-            model[indexPath.section].isExpanded.toggle()
-            tableView.reloadSections(IndexSet(integer: indexPath.section), with: .automatic)
-        } else if indexPath.row == 1 {
+        model[indexPath.section].isExpanded.toggle()
+        if indexPath.row != 0 {
             let selectedOption = model[indexPath.section].options[indexPath.row - 1]
-            // go to default page
-            performSegue(withIdentifier: "showController2", sender: selectedOption)
-        } else {
-            
-//            go to edit custom  page
+            model[indexPath.section].selectedOption = selectedOption
+        }
+        tableView.reloadSections(IndexSet(integer: indexPath.section), with: .automatic)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 181
+        }else {
+            return 80
         }
     }
+    
+    @IBAction func CheckResultTapped(_ sender: Any) {
+        var identifier: String = ""
+        if model[1].selectedOption == "Custom" {
+            identifier = "compatibilityToCustomWeight"
+        } else {
+            identifier = "CompatibilityToResults"
+        }
+        performSegue(withIdentifier: identifier, sender: nil)
+    }
+    
 
     // Prepare for segue to Controller 2
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showController2", let destinationVC = segue.destination as? compatabilityResult, let selectedOption = sender as? String {
-            // Set the delegate to CheckCompatibilityViewController
-            destinationVC.delegate = self // Ensure the delegate is set to CheckCompatibilityViewController
-            // Pass the selected option to Controller 2
-            destinationVC.selectedOption = selectedOption
+        if segue.identifier == "compatibilityToResult" {
+            let vc = segue.destination as! compatabilityResult
+            vc.application =  model[0].selectedOption
         }
     }
 }
